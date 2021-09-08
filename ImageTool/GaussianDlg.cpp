8534607d764bebc4824fe65ea02a4fc0443af5ad
Lee-Seungwook <lsw971226@naver.com>
2021-09-08 15:18:26 +0900

    가우시안 다이얼로그 구현

--- ImageTool/GaussianDlg.cpp
+++ ImageTool/GaussianDlg.cpp
@@ -31,8 +31,55 @@ void CGaussianDlg::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(CGaussianDlg, CDialogEx)
+	ON_WM_HSCROLL()
+	ON_EN_CHANGE(IDC_SIGMA_EDIT, &CGaussianDlg::OnChangeSigmaEdit)
 END_MESSAGE_MAP()
 
 
 // CGaussianDlg 메시지 처리기
 
+
+
+BOOL CGaussianDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+
+	// 슬라이더 컨트롤의 초기화
+	m_sliderSigma.SetRange(10, 250);
+	m_sliderSigma.SetTicFreq(20);
+	m_sliderSigma.SetPageSize(20);
+	m_sliderSigma.SetPos(static_cast<int>(m_fSigma * 50));
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+void CGaussianDlg::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	// 슬라이드바에서 발생한 WM_HSCROLL 메시지인 경우 처리
+	if (m_sliderSigma.GetSafeHwnd() == pScrollBar->GetSafeHwnd())
+	{
+		int pos = m_sliderSigma.GetPos();
+		m_fSigma = (pos / 50.0f);
+		UpdateData(FALSE);
+	}
+	CDialogEx::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CGaussianDlg::OnChangeSigmaEdit()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CDialogEx::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_sliderSigma.SetPos(static_cast<int>(m_fSigma * 50));
+}
