159cc00761d871432e8b5398539e6eaa778c8c36
Lee-Seungwook <lsw971226@naver.com>
2021-09-09 09:44:13 +0900

    미디언 필터 함수 선언 및 정의

--- ImageTool/IppFilter.cpp
+++ ImageTool/IppFilter.cpp
@@ -266,3 +266,32 @@ void IppNoiseSaltNPepper(IppByteImage& imgSrc, IppByteImage& imgDst, int amount)
 		pDst[distribution(generator)] = (i & 0x01) * 255; // 잡음이 추가될 좌표
 	}
 }
+
+
+// 미디언 필터 구현
+void IppFilterMedian(IppByteImage& imgSrc, IppByteImage& imgDst)
+{
+	int w = imgSrc.GetWidth();
+	int h = imgSrc.GetHeight();
+
+	imgDst = imgSrc;
+
+	BYTE** pSrc = imgSrc.GetPixels2D();
+	BYTE** pDst = imgDst.GetPixels2D();
+
+	int i, j;
+	BYTE m[9];
+	
+	for (j = 1; j < h - 1; j++) // 중위 수를 계산하기 위한 반복문 밑 sort 함수 적용
+	for (i = 1; i < w - 1; i++)
+	{
+		m[0] = pSrc[j - 1][i - 1]; m[1] = pSrc[j - 1][i]; m[2] = pSrc[j - 1][i + 1];
+		m[3] = pSrc[j][i - 1]; m[4] = pSrc[j][i]; m[5] = pSrc[j][i + 1];
+		m[6] = pSrc[j + 1][i - 1]; m[7] = pSrc[j + 1][i]; m[8] = pSrc[j + 1][i + 1];
+
+		std::sort(m, m + 9); // 주소를 매개변수로 사용하기 때문에 배열의 주소를 넘겨준다.
+
+		pDst[j][i] = m[4];
+	}
+
+}
--- ImageTool/IppFilter.h
+++ ImageTool/IppFilter.h
@@ -2,6 +2,7 @@
 
 #include "IppImage/IppImage.h"
 
+// 필터 구현
 // 평균 값 필터 구현
 void IppFilterMean(IppByteImage& imgSrc, IppByteImage& imgDst); // imgSrc 영상에 3 x 3 크기의 마스크를 이용한 평균값 필터링을 수행, imgDst에 저장
 
@@ -20,9 +21,14 @@ void IppFilterUnsharpMask(IppByteImage& imgSrc, IppByteImage& imgDst);
 // 하이부스트 필터
 void IppFilterHighboost(IppByteImage& imgSrc, IppByteImage& imgDst, float alpha);
 
-
+// 잡음 추가
 // 가우시안 잡음 추가
 void IppNoiseGaussian(IppByteImage& imgSrc, IppByteImage& imgDst, int amount);
 
 // 소금, 후추 잡음 추가
 void IppNoiseSaltNPepper(IppByteImage& imgSrc, IppByteImage& imgDst, int amount);
+
+
+// 잡음 제거 
+// 미디언 필터링 함수 추가 
+void IppFilterMedian(IppByteImage& imgSrc, IppByteImage& imgDst);
